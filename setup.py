1dbbee2a631cf412af8f87f602ca2700e0c98738
Sam Woodman <sam.woodman@noaa.gov>
2022-10-26 20:08:37 -0600

    push setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='gdm',
+      version='0.0',
+      description='',
+      url='http://github.com/kerfoot/gdm',
+      author='John Kerfoot',
+      author_email='jkerfoot@marine.rutgers.edu',
+      license='GPL3.0',
+      packages=['gdm'],
+      install_requires=[
+            'gsw', 
+            'netcdf4', 
+            'numpy', 
+            'pandas', 
+            'scipy', 
+            'shapely', 
+            'yaml', #not available through pip
+            'xarray'
+      ],
+      zip_safe=False)
