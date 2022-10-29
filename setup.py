ccd51f2f9ae235b5b1ff5a74e252f1dba643ceb2
Sam Woodman <sam.woodman@noaa.gov>
2022-10-29 10:21:46 -0400

    clean up setup and env

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from setuptools import setup
+from gdm._version import __version__
 
 setup(name='gdm',
-      version='0.0',
-      description='',
+      version=__version__,
+      description='Universal data model for profiling ocean gliders',
       url='http://github.com/kerfoot/gdm',
       author='John Kerfoot',
       author_email='jkerfoot@marine.rutgers.edu',
       license='GPL3.0',
       packages=['gdm'],
+      python_requires='>=3.8.5',
       install_requires=[
             'gsw', 
             'netcdf4', 
             'numpy', 
             'pandas', 
+            'pyyaml',
             'scipy', 
             'shapely', 
-            'pyyaml',
             'xarray'
       ],
       zip_safe=False)
